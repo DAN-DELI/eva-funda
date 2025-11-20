2070923bd6a67a2445e95a35c2f591dee08f0c50
Jhon Bueno <jhonbuenoa@gmail.com>
2025-11-20 17:21:54 -0500

    Agrega unicamente el array en la consola

--- ejercicio/ejerciciotres.js
+++ ejercicio/ejerciciotres.js
@@ -9,23 +9,7 @@ let numero = parseInt(prompt("Ingrese el número base de la tabla de multiplicar
 // aplica la función
 let resultados = generarTabla(numero);
 
-console.log("\n=== IMPRESION EN CONSOLE.LOG ===")
-// Mostrar en consola
-/*Ponemos este console log aparte, para que no quede dentro del bucle
-y se muestre acada rato, tabla del numero tal*/
-console.log(`Tabla del ${numero}:`);
-/*Se usa un bucle for...of para recorrer el arreglo 'resultados' y mostrar 
-cada operación de la tabla.
-valor es el resultado de cada multiplicación, "i", es el indice del arreglo, le asignamos
-manualmente que valga 1, para que no comience en cero.
-*/
-let i = 1;
-for (let valor of resultados) {
-console.log(`${i} x ${numero} = ${valor}`);
-i++;
-}
-
-console.log("\n=== IMPRESION EN ARREGLO ===")
+console.log("=== IMPRESION EN ARREGLO ===")
 
 // Mostrar en consola
 console.log(`Tabla del ${numero}:`, resultados);
